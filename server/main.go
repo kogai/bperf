bd1cd69851dd24eb216ee0ef4923ddf13439a25a
Shinichi Kogai <kogai0121@gmail.com>
2019-04-22 15:20:39 +0900

    tmp

--- server/main.go
+++ server/main.go
@@ -2,21 +2,44 @@ package main
 
 // import "log"
 // 	log.New()
-import "time"
-import "fmt"
-import "os"
-import "net/http"
-import "encoding/json"
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+)
+
+func toJSON(m interface{}) []byte {
+	js, err := json.Marshal(m)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return js
+}
+
+type beacon struct {
+	Time float64 `json:"time"`
+}
 
 func send(w http.ResponseWriter, r *http.Request) {
 	type MyRes struct {
 		A string `json:"a"`
 	}
+
 	res, _ := json.Marshal(MyRes{A: "aaa"})
 
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	time.Sleep(time.Duration(1 * time.Second)) // Emulate distance of remote server.
+	// time.Sleep(time.Duration(1 * time.Second)) // Emulate distance of remote server.
+
+	js, _ := url.ParseQuery(r.URL.RawQuery)
+	bytes, _ := json.Marshal(js)
+	var b beacon
+	_ = json.Unmarshal(bytes, &b)
+	fmt.Printf("Time %v", b.Time)
 
 	_, err := w.Write(res)
 	if err != nil {
@@ -31,7 +54,16 @@ func main() {
 		port = p
 	}
 	fmt.Printf("Server just started at PORT=%s\n", port)
+	js, _ := url.ParseQuery("time=99")
 
-	http.HandleFunc("/send", send)
+	fmt.Printf("%v %v\n", js, js.Get("time"))
+	bytes, _ := json.Marshal(js)
+	var b beacon
+	e := json.Unmarshal(bytes, &b)
+
+	fmt.Printf("E %v\n", e)
+	fmt.Printf("Time %v", b.Time)
+
+	http.HandleFunc("/beacon", send)
 	http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
 }
