b38378d69a72d5da1db72031d23299126442ba11
Shinichi Kogai <kogai0121@gmail.com>
2019-04-23 22:23:17 +0900

    :construction: Attempt to connect psql server

--- server/main.go
+++ server/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
 	"strconv"
+
+	_ "github.com/lib/pq"
 )
 
 type beacon struct {
@@ -13,7 +16,19 @@ type beacon struct {
 	Time      int64  `json:"time"`
 }
 
+var conn *sql.DB
 var db = make([]beacon, 0)
+
+func initDb() {
+	user := ensureEnv("DB_USER", nil)
+	dbname := ensureEnv("DB_DATABASE", nil)
+	password := ensureEnv("DB_PASSWORD", nil)
+	var err error
+	conn, err = sql.Open("postgres", fmt.Sprintf("user=%s dbname=%s password=%s sslmode=disable", user, dbname, password))
+	if err != nil {
+		panic(err)
+	}
+}
 
 func beaconHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
@@ -42,10 +57,26 @@ func eventsHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func ensureEnv(name string, defaultValue interface{}) string {
+	v := os.Getenv(name)
+
+	if v == "" && nil == defaultValue {
+		panic(fmt.Sprintf("An environment variable [%s] must be defined.", name))
+	}
+	if v == "" && nil != defaultValue {
+		return defaultValue.(string)
+	}
+	return v
+}
+
 func main() {
-	port := "5000"
-	if p := os.Getenv("PORT"); p != "" {
-		port = p
+	var err error
+	port := ensureEnv("PORT", "5000")
+	initDb()
+
+	_, err = conn.Query("SELECT name FROM users WHERE email = $1", "a@a.com")
+	if err != nil {
+		panic(err)
 	}
 
 	http.HandleFunc("/events", eventsHandler)
@@ -55,7 +86,7 @@ func main() {
 	})
 
 	fmt.Printf("API Server has been started at :%s\n", port)
-	err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+	err = http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
 	if err != nil {
 		panic(err)
 	}
