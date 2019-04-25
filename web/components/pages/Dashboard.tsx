f9762aaa1e4bb66eef2c19de2664ebd9ccccaf45
Shinichi Kogai <kogai0121@gmail.com>
2019-04-25 22:48:52 +0900

    :lipstick: Introduce bar chart

--- web/components/pages/Dashboard.tsx
+++ web/components/pages/Dashboard.tsx
@@ -1,12 +1,36 @@
 import React, { ComponentType } from "react";
 import { Link } from "react-router-dom";
+import { Bar } from "react-chartjs-2";
 import styled, { StyledComponent } from "styled-components";
 import { Event } from "../../modules/http";
+
+const data = {
+  labels: ["January", "February", "March", "April", "May", "June", "July"],
+  datasets: [
+    {
+      label: "My First dataset",
+      fillColor: "rgba(220,220,220,0.5)",
+      strokeColor: "rgba(220,220,220,0.8)",
+      highlightFill: "rgba(220,220,220,0.75)",
+      highlightStroke: "rgba(220,220,220,1)",
+      data: [65, 59, 80, 81, 56, 55, 40]
+    },
+    {
+      label: "My Second dataset",
+      fillColor: "rgba(151,187,205,0.5)",
+      strokeColor: "rgba(151,187,205,0.8)",
+      highlightFill: "rgba(151,187,205,0.75)",
+      highlightStroke: "rgba(151,187,205,1)",
+      data: [28, 48, 40, 19, 86, 27, 90]
+    }
+  ]
+};
 
 export type Props = { events: Event[]; onClick: () => void };
 
 export const Dashboard: ComponentType<Props> = ({ events, onClick }) => (
   <>
+    <Bar data={data} />
     <table>
       <tbody>
         <tr>
