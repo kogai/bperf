c2d20a4548e5fb3f9d6cedc7fe74d5d9f874d916
shinichi kogai <kogai0121@gmail.com>
2019-04-26 17:50:51 +0900

    :lipstick: Make Bar-chart available by Elm

--- src/Main.elm
+++ src/Main.elm
@@ -1,36 +1,121 @@
-module Main exposing (Msg(..), main, update, view)
+-- module Main exposing (Msg(..), main, update, view)
+-- import Browser
+-- import Html exposing (Html, button, div, text)
+-- import Html.Events exposing (onClick)
+-- main =
+--     Browser.sandbox { init = 0, update = update, view = view }
+-- type Msg
+--     = Increment
+--     | Decrement
+--     | Something Int
+-- update msg model =
+--     case msg of
+--         Increment ->
+--             model + 1
+--         Decrement ->
+--             model - 1
+--         Something x ->
+--             model + x
+-- view model =
+--     div []
+--         [ button [ onClick Decrement ] [ text "-" ]
+--         , div [] [ text (String.fromInt model) ]
+--         , button [ onClick Increment ] [ text "+" ]
+--         , button [ onClick (Something 10) ] [ text "10" ]
+--         ]
 
-import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+
+module Main exposing (main)
+
+import Axis
+import DateFormat
+import SampleData exposing (timeSeries)
+import Scale exposing (BandConfig, BandScale, ContinuousScale, defaultBandConfig)
+import Time
+import TypedSvg exposing (g, rect, style, svg, text_)
+import TypedSvg.Attributes exposing (class, textAnchor, transform, viewBox)
+import TypedSvg.Attributes.InPx exposing (height, width, x, y)
+import TypedSvg.Core exposing (Svg, text)
+import TypedSvg.Types exposing (AnchorAlignment(..), Transform(..))
+
+
+w : Float
+w =
+    900
+
+
+h : Float
+h =
+    450
+
+
+padding : Float
+padding =
+    30
+
+
+xScale : List ( Time.Posix, Float ) -> BandScale Time.Posix
+xScale model =
+    List.map Tuple.first model
+        |> Scale.band { defaultBandConfig | paddingInner = 0.1, paddingOuter = 0.2 } ( 0, w - 2 * padding )
+
+
+yScale : ContinuousScale Float
+yScale =
+    Scale.linear ( h - 2 * padding, 0 ) ( 0, 5 )
+
+
+dateFormat : Time.Posix -> String
+dateFormat =
+    DateFormat.format [ DateFormat.dayOfMonthFixed, DateFormat.text " ", DateFormat.monthNameAbbreviated ] Time.utc
+
+
+xAxis : List ( Time.Posix, Float ) -> Svg msg
+xAxis model =
+    Axis.bottom [] (Scale.toRenderable dateFormat (xScale model))
+
+
+yAxis : Svg msg
+yAxis =
+    Axis.left [ Axis.tickCount 5 ] yScale
+
+
+column : BandScale Time.Posix -> ( Time.Posix, Float ) -> Svg msg
+column scale ( date, value ) =
+    g [ class [ "column" ] ]
+        [ rect
+            [ x <| Scale.convert scale date
+            , y <| Scale.convert yScale value
+            , width <| Scale.bandwidth scale
+            , height <| h - Scale.convert yScale value - 2 * padding
+            ]
+            []
+        , text_
+            [ x <| Scale.convert (Scale.toRenderable dateFormat scale) date
+            , y <| Scale.convert yScale value - 5
+            , textAnchor AnchorMiddle
+            ]
+            [ text <| String.fromFloat value ]
+        ]
+
+
+view : List ( Time.Posix, Float ) -> Svg msg
+view model =
+    svg [ viewBox 0 0 w h ]
+        [ style [] [ text """
+            .column rect { fill: rgba(118, 214, 78, 0.8); }
+            .column text { display: none; }
+            .column:hover rect { fill: rgb(118, 214, 78); }
+            .column:hover text { display: inline; }
+          """ ]
+        , g [ transform [ Translate (padding - 1) (h - padding) ] ]
+            [ xAxis model ]
+        , g [ transform [ Translate (padding - 1) padding ] ]
+            [ yAxis ]
+        , g [ transform [ Translate padding padding ], class [ "series" ] ] <|
+            List.map (column (xScale model)) model
+        ]
 
 
 main =
-    Browser.sandbox { init = 0, update = update, view = view }
-
-
-type Msg
-    = Increment
-    | Decrement
-    | Something Int
-
-
-update msg model =
-    case msg of
-        Increment ->
-            model + 1
-
-        Decrement ->
-            model - 1
-
-        Something x ->
-            model + x
-
-
-view model =
-    div []
-        [ button [ onClick Decrement ] [ text "-" ]
-        , div [] [ text (String.fromInt model) ]
-        , button [ onClick Increment ] [ text "+" ]
-        , button [ onClick (Something 10) ] [ text "10" ]
-        ]
+    view timeSeries
