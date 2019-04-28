6f892c256721e1b8ecd77294d08499913584c245
Shinichi Kogai <kogai0121@gmail.com>
2019-04-28 15:08:27 +0900

    :construction: Attempt to compose Page

--- web/Main.elm
+++ web/Main.elm
@@ -2,24 +2,12 @@ module Main exposing (main)
 
 import Browser
 import Browser.Navigation
-import HistogramChart
 import Html exposing (Html, a, div, li, text, ul)
 import Html.Attributes exposing (href)
-import Http
-import Json.Decode as Decode exposing (Decoder, float, list, string)
-import Json.Decode.Pipeline exposing (required)
-import TypedSvg.Core exposing (text)
+import Page.Dashboard
 import TypedSvg.Types exposing (AnchorAlignment(..), Transform(..))
 import Url
-import View.Dashboard
 import View.SignIn
-
-
-
--- type Model
---     = Failure
---     | Loading
---     | Success Events
 
 
 type alias Model =
@@ -34,72 +22,14 @@ type alias Event =
     }
 
 
-type alias Events =
-    List Event
-
-
-eventDecoder : Decoder Event
-eventDecoder =
-    Decode.succeed Event
-        |> required "time" float
-        |> required "eventType" string
-
-
-eventsDecoder : Decoder (List Event)
-eventsDecoder =
-    Decode.list eventDecoder
-
-
-
--- type Msg
---     = GotText (Result Http.Error Events)
-
-
 init : () -> Url.Url -> Browser.Navigation.Key -> ( Model, Cmd Msg )
-init flags url key =
+init _ url key =
     ( Model key url, Cmd.none )
-
-
-
--- init : () -> ( Model, Cmd Msg )
--- init _ =
---     ( Loading
---     , Http.get
---         { url = "http://localhost:5000/events"
---         , expect = Http.expectJson GotText eventsDecoder
---         }
---     )
--- update : Msg -> Model -> ( Model, Cmd Msg )
--- update msg _ =
---     case msg of
---         GotText result ->
---             case result of
---                 Ok fullText ->
---                     ( Success fullText, Cmd.none )
---                 Err _ ->
---                     ( Failure, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
     Sub.none
-
-
-
--- my_view : Model -> Html Msg
--- my_view model =
---     case model of
---         Failure ->
---             text "Unable to load events"
---         Loading ->
---             text "Loading..."
---         Success fullText ->
---             HistogramChart.view <| List.map (\event -> event.time) fullText
--- root_view : Model -> Html Msg
--- root_view model =
---     div []
---         [ my_view model
---         ]
 
 
 type Msg
@@ -144,7 +74,7 @@ view model =
                 ]
 
             "/dashboard" ->
-                [ View.Dashboard.frame () ]
+                [ Page.Dashboard.view () ]
 
             "/sign_in" ->
                 [ View.SignIn.frame () ]
---
+++ web/Page/Dashboard.elm
@@ -0,0 +1,76 @@
+module Page.Dashboard exposing (init, update, view)
+
+import Html exposing (Html, text)
+import Http
+import Json.Decode as Decode exposing (Decoder, float, list, string)
+import Json.Decode.Pipeline exposing (required)
+import TypedSvg.Core exposing (text)
+import TypedSvg.Types exposing (AnchorAlignment(..), Transform(..))
+import View.Dashboard
+
+
+type Model
+    = Failure
+    | Loading
+    | Success Events
+
+
+type alias Event =
+    { time : Float
+    , eventType : String -- FIXME: Define as Custom type
+    }
+
+
+type alias Events =
+    List Event
+
+
+eventDecoder : Decoder Event
+eventDecoder =
+    Decode.succeed Event
+        |> required "time" float
+        |> required "eventType" string
+
+
+eventsDecoder : Decoder (List Event)
+eventsDecoder =
+    Decode.list eventDecoder
+
+
+type Msg
+    = GotText (Result Http.Error Events)
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( Loading
+    , Http.get
+        { url = "http://localhost:5000/events"
+        , expect = Http.expectJson GotText eventsDecoder
+        }
+    )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg _ =
+    case msg of
+        GotText result ->
+            case result of
+                Ok fullText ->
+                    ( Success fullText, Cmd.none )
+
+                Err _ ->
+                    ( Failure, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    case model of
+        Failure ->
+            text "Unable to load events"
+
+        Loading ->
+            text "Loading..."
+
+        Success fullText ->
+            View.Dashboard.view <| List.map (\event -> event.time) fullText
