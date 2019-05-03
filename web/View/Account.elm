970b244ec5f0429a40efac365619bec3e529b4d5
Shinichi Kogai <kogai0121@gmail.com>
2019-05-03 14:11:00 +0900

    :recycle: Commonize table row

--- web/View/Account.elm
+++ web/View/Account.elm
@@ -3,9 +3,20 @@ module View.Account exposing (view)
 import Api.AccountDetail as Api
 import Css exposing (pct, width)
 import Html as UnStyled
-import Html.Styled exposing (div, h1, li, table, tbody, td, text, th, thead, toUnstyled, tr, ul)
+import Html.Styled exposing (Html, div, h1, li, table, tbody, td, text, th, thead, toUnstyled, tr, ul)
 import Html.Styled.Attributes exposing (class, css)
 import View.Template.Common as Layout
+
+
+tableRow : String -> String -> Html msg
+tableRow k v =
+    tr []
+        [ th []
+            [ text k ]
+        , td
+            []
+            [ text v ]
+        ]
 
 
 view : Api.Response -> UnStyled.Html msg
@@ -19,21 +30,8 @@ view { privilege, mail } =
                 , th [] [ text "value" ]
                 ]
             , tbody []
-                [ tr []
-                    [ th []
-                        [ text "product name" ]
-                    , td
-                        []
-                        [ text "experimental-app" ]
-                    ]
-                , tr []
-                    [ th []
-                        [ text "script tag" ]
-                    , td
-                        []
-                        [ text "<script src=\"https://beacon.bperf.com?key=aaabbbccc\" />"
-                        ]
-                    ]
+                [ tableRow "product name" "experimental-app"
+                , tableRow "script tag" "<script src=\"https://beacon.bperf.com?key=aaabbbccc\" />"
                 , tr []
                     [ th []
                         [ text "monitoring targets" ]
@@ -45,13 +43,7 @@ view { privilege, mail } =
                             ]
                         ]
                     ]
-                , tr []
-                    [ th []
-                        [ text "pricing plan" ]
-                    , td
-                        []
-                        [ text "free" ]
-                    ]
+                , tableRow "pricing plan" "free"
                 ]
             ]
         , h1 [ class "title is-3" ] [ text "User information" ]
@@ -61,20 +53,8 @@ view { privilege, mail } =
                 , th [] [ text "value" ]
                 ]
             , tbody []
-                [ tr []
-                    [ th []
-                        [ text "your privilege" ]
-                    , td
-                        []
-                        [ text privilege ]
-                    ]
-                , tr []
-                    [ th []
-                        [ text "mail address" ]
-                    , td
-                        []
-                        [ text mail ]
-                    ]
+                [ tableRow "your privilege" privilege
+                , tableRow "mail address" mail
                 ]
             ]
         ]
